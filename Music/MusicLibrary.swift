45c90d120953faf385f0c2b5f31fd67afd2e1f05
Nikita Paralkar <44620564+paralkarniki@users.noreply.github.com>
2019-12-12 22:09:11 -0500

    Add files via upload

---
+++ Music/MusicLibrary.swift
@@ -0,0 +1,15 @@
+//
+//  MusicLibrary.swift
+//  project
+//
+//  Created by  Nikita Paralkar  on 11/29/19.
+//  Copyright © 2019 Vandan Inc. All rights reserved.
+//
+
+import Foundation
+
+struct MusicLibrary{
+    
+    var library = [["title":"Music","artist":"Apple","index":"0","coverImage":"0" ], ["title":"Follow God","artist":"Kanye West","index":"1","coverImage":"1" ], ["title":"Faster Harder Stronger","artist":"Kanye West","index":"2","coverImage":"2" ]]
+    
+}
---
+++ Music/SiteCell.swift
@@ -0,0 +1,56 @@
+//
+//  SiteCell.swift
+//  p_3
+//
+//  Created by  Nikita Paralkar  on 10/8/19.
+//  Copyright © 2019 Vandan Inc. All rights reserved.
+//
+
+import UIKit
+
+class SiteCell: UITableViewCell {
+    
+    let primaryLabel = UILabel()
+    let secondaryLabel = UILabel()
+    let myImageView = UIImageView()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        primaryLabel.textAlignment = .left
+        primaryLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        primaryLabel.backgroundColor = .clear
+        primaryLabel.textColor = .black
+        
+        secondaryLabel.textAlignment = .left
+        secondaryLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        secondaryLabel.backgroundColor = .clear
+        secondaryLabel.textColor = .black
+        
+        super .init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(primaryLabel)
+        contentView.addSubview(secondaryLabel)
+        contentView.addSubview(myImageView)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        primaryLabel.frame = CGRect(x: 100, y: 5, width: 460, height: 30)
+        secondaryLabel.frame = CGRect(x: 100, y: 40, width: 460, height: 20)
+        myImageView.frame = CGRect(x: 5, y: 5, width: 45, height: 45)
+        
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
